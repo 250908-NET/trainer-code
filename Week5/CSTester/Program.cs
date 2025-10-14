d08651e31f1f16da796a2acf8a615b47e75c53d3
Richard Hawkins <Richard.Hawkins@Revature.com>
2025-10-14 17:03:08 -0400

    MORE updates to the Docker Demo!

--- Week5/CSTester/Program.cs
+++ Week5/CSTester/Program.cs
@@ -1,26 +1,37 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// builder.Configuration.AddJsonFile("values.json", optional: true, reloadOnChange: true);
-
-builder.Services.AddOpenApi();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-app.MapOpenApi();
-app.UseSwagger();
-app.UseSwaggerUI();
-
-app.UseHttpsRedirection();
-
-app.MapGet("/", () => "Hello World!");
-
-app.MapGet("/string", (IConfiguration config) =>
+namespace csTester
+{
+    public class Program
     {
-        var CS = config.GetConnectionString("MyConnectionString");
-        return Results.Ok(CS);
-    }
-);
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-app.Run();
+            builder.Configuration.AddJsonFile("values.json", optional: true, reloadOnChange: true);
+
+            builder.Services.AddOpenApi();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // options.ListenAnyIp(80);
+
+            var app = builder.Build();
+
+            app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            app.UseHttpsRedirection();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.MapGet("/string", (IConfiguration config) =>
+                {
+                    var CS = config.GetConnectionString("MyConnectionString");
+                    return Results.Ok(CS);
+                }
+            );
+
+            app.Run();
+        }
+    }
+}
